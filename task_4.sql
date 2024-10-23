3b9e49fc50a3eab74338b2e8f0a343291eb850b8
Omar Fathy <omar.f.shoman@gmail.com>
2024-10-23 19:10:56 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,15 @@
+-- task_4.sql
 USE alx_book_store;
 
-DESC books;
-
-SELECT COLUMN_NAME, COLUMN_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books' ;
